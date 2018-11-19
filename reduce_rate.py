7e1ad1db3f41f7b7659263c543db226191e310e2
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2018-11-19 14:36:27 -0500

    change to data reduction script to make it better and actually work

--- reduce_rate.py
+++ reduce_rate.py
@@ -1,21 +1,26 @@
 import os
 from pydub import AudioSegment as AS
 from pydub.playback import play
+import sys
 
+src = sys.argv[1]
+dest = sys.argv[2]
+frame_rate = int(sys.argv[3])
+count = 0
 
-
-
-for root, dirs, files in os.walk("./data"):
+for root, dirs, files in os.walk(src):
     f = [root+'/'+i for i in files if i.endswith(".mp3")]
-
     for song in f:
-        if os.path.isfile(song.replace("data", "low_data")):
+        if os.path.isfile(song.replace(src, dest)):            
             continue
-        if not os.path.isdir(root.replace("./data", "./low_data")):
-            os.system("mkdir " + root.replace("./data", "./low_data"))
+        if not os.path.isdir(root.replace(src, dest)):
+            os.system("mkdir " + root.replace(src, dest))
         try:
-            music = AS.from_mp3(song)
-            music.export(song.replace("./data", "./low_data"), format='mp3', bitrate='1k')
+            music = AS.from_mp3(song).set_frame_rate(frame_rate)
+            music.export(song.replace(src, dest), format='mp3')
         except:
             continue
     print(root)
+    if count > 1:
+        break
+    count += 1
